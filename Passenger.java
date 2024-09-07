c57c2d15377da0227ba10083ba59a7d9f196e9b7
ArchanaAnguraj <archanaat12345@gmail.com>
2024-09-07 19:51:21 +0530

    Add files via upload

---
+++ Passenger.java
@@ -0,0 +1,21 @@
+package com.zoho.advance.Railway;
+
+public class Passenger {
+	String name;
+	int age;
+	String gender;
+	String birthPrefarance;
+	
+	
+	public Passenger(String name, int age, String gender, String birthPrefarance) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.birthPrefarance = birthPrefarance;
+	}
+	
+	
+	
+
+}
